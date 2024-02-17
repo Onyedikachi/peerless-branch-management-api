02bb4df1a99638438f35e8f87fe999e4443302c7
stephen-ohaeri <stephen@bepeerless.co>
2024-02-17 15:19:17 +0100

    added Auditlog schema

---
+++ Retail.Branch.Core/Entities/AuditLogs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Branch.Core.Entities
+{
+    public class AuditLogs
+    {
+        [Key]
+        public string User_Id { get; set; }
+        public string User_Name { get; set; }
+        public string Action_Type { get; set; }
+        public int Clicks { get; set; }
+        public string status { get; set; } = "false";
+        public string SourceIPs { get; set; }
+        public string BranchCode { get; set; }
+        public DateTime LoginTime { get; set; }
+        public DateTime Created_At { get; set; } = DateTime.UtcNow;
+    }
+}
